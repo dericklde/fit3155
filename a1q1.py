92df81e15062cedc900f52eb7e7449759bf4f5da
Leu Douh Ee <dleu0005@student.monash.edu>
2025-03-30 23:15:45 +1100

    figured out indexing for left case, right case and middle case for successive char swap false positive case also included

--- a1q1.py
+++ a1q1.py
@@ -43,11 +43,11 @@ def z_algo(pattern, text):
         suffix_len = z2[n - i + 1]
 
         if prefix_len + suffix_len == (m - 1):
-            print("i = ", i, "pref = ", prefix_len)
-            print("i = ", i, "suff = ", suffix_len)
+            # print("i = ", i, "pref = ", prefix_len)
+            # print("i = ", i, "suff = ", suffix_len)
             replace_char.append(i)
     
-    print(replace_char)
+    # print(replace_char)
 
     insert_char = []
 
@@ -56,11 +56,11 @@ def z_algo(pattern, text):
         suffix_len = z2[n - i + 2]
 
         if prefix_len + suffix_len == (m - 1):
-            print("i = ", i, "pref = ", prefix_len)
-            print("i = ", i, "suff = ", suffix_len)
+            # print("i = ", i, "pref = ", prefix_len)
+            # print("i = ", i, "suff = ", suffix_len)
             insert_char.append(i)
     
-    print(insert_char)
+    # print(insert_char)
 
     delete_char = []
 
@@ -69,15 +69,46 @@ def z_algo(pattern, text):
         suffix_len = z2[n - i]
 
         if prefix_len + suffix_len == m: 
-            print("i = ", i, "pref = ", prefix_len)
-            print("i = ", i, "suff = ", suffix_len)
+            # print("i = ", i, "pref = ", prefix_len)
+            # print("i = ", i, "suff = ", suffix_len)
             delete_char.append(i)
     
-    print(delete_char)
-    # res = []
-    # for i in range(len(new_text)):
-    #     if z[i] == len(pattern):
-    #         res.append(i - (len(pattern)+1)) # +1 for delimiter "$"
+    # print(delete_char)
+
+    print("z1: ",z1)
+    print("z2: ",z2)
+    swap_char = []
     
-    # return res
-print(z_algo("abcd", "axbcdabxcdabcxd"))
+    for i in range(n - m + 1):
+        prefix_len = z1[i + m + 1]
+        suffix_len = z2[n - i + 1]
+
+        if prefix_len + suffix_len == m - 2:
+            print("i = ", i, "pref = ", prefix_len)
+            print("i = ", i, "suff = ", suffix_len)
+            if prefix_len < suffix_len:
+                print("left case", prefix_check[i + m + 1 + prefix_len], prefix_check[i + m + 2 + prefix_len])
+                print("pattern: ", pattern[prefix_len], pattern[prefix_len + 1])
+                if prefix_check[i + m + 1 + prefix_len] == pattern[prefix_len + 1] and prefix_check[i + m + 2 + prefix_len] == pattern[prefix_len]:
+                    swap_char.append(i)
+            elif prefix_len > suffix_len:
+                print("right case", suffix_check[n - i + 2 + suffix_len], suffix_check[n - i + 1 + suffix_len])
+                print("pattern: ", pattern[m-1-suffix_len-1], pattern[m-1-suffix_len])
+                if suffix_check[n - i + 2 + suffix_len] == pattern[m-1-suffix_len] and suffix_check[n - i + 1 + suffix_len] == pattern[m-1-suffix_len-1]:
+                    swap_char.append(i)
+            elif prefix_len == suffix_len:
+                print("middle case", prefix_check[i + m + 1 + prefix_len], suffix_check[n - i + 1 + suffix_len])
+                print("pattern: ", pattern[prefix_len], pattern[m-1-suffix_len])
+                if prefix_check[i + m + 1 + prefix_len] == pattern[m-1-suffix_len] and suffix_check[n - i + 1 + suffix_len] == pattern[prefix_len]:
+                    swap_char.append(i)
+            else:
+                print("false positive")
+                continue
+    
+    print(swap_char)
+
+
+
+print(z_algo("abcd", "bacdacbdabdc"))
+             # abcd # bacd acbd abdc dbca - forward
+             # dcba # acbd cdba dbca dcab - reverse
